160695112f24d2bb322bc0db70491001156d7370
Murray <KATE.MURRAY@VIZIENTINC.COM>
2020-03-02 15:41:03 -0600

    working on tests for message service

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,20 @@
+import { StrengthPipe } from "./strength.pipe";
+
+describe("strength pipe", () => {
+  it("should display weak if strength is 5", () => {
+    //arrange
+    let pipe = new StrengthPipe();
+
+    let val = pipe.transform(5);
+
+    expect(val).toEqual("5 (weak)");
+  });
+
+  it("should display strong ig strength is 10", () => {
+    let pipe = new StrengthPipe();
+
+    let val = pipe.transform(10);
+
+    expect(val).toEqual("10 (strong)");
+  });
+});
