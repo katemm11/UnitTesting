c0c99d507d9186b6c6cb35b4ce3f03b76130aa8f
Murray <KATE.MURRAY@VIZIENTINC.COM>
2020-03-03 08:38:49 -0600

    finished message service test

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -6,16 +6,33 @@ describe("MessageService", () => {
   beforeEach(() => {});
 
   it("should have no messages to start", () => {
+    //arrange + act
     service = new MessageService();
 
+    //assert
     expect(service.messages.length).toBe(0);
   });
 
   it("should add a message when add is called", () => {
+    //arrange
     service = new MessageService();
 
+    //act
     service.add("message1");
 
+    //assert
     expect(service.messages.length).toBe(1);
+  });
+
+  it("should remove all messages when clear is called", () => {
+    //arrange
+    service = new MessageService();
+    service.add("message1");
+
+    //act
+    service.clear();
+
+    //assert
+    expect(service.messages.length).toBe(0);
   });
 });
